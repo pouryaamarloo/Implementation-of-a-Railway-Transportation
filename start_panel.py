b8e0a868515fb6848556c6cdae184ed692ec8d71
pourya <pouryaamarloo162@gmail.com>
2025-09-15 21:01:39 +0330

    The first structure for simplifying Group 1’s module

---
+++ start_panel.py
@@ -0,0 +1,29 @@
+def start_panel():
+    '''
+    پنل شروع جهت فراخوانی پنل های متفاوت از قبیل
+    ادمین کل ، کارمند قطار، کاربر عادی
+    '''
+    state =0
+    while True:
+        print("1.ادمین کل")
+        print("2.کارمند قطار")
+        print("3.کاربر عادی")
+        print("4.خروج")
+        user_input = int(input())
+
+        if user_input == 1:
+            user_name = input()
+            password = input()
+            if user_name == "Admin_Train" :
+                state = 1
+            else :
+                print("Error wrong user name")
+                continue
+            if password == "Pass_Train" and state == 1 :
+                admin = Admin_Panel()    # فراخوانی کلاس ادمین کل
+                print("خوش اومدی ادمین")
+            else :
+                print("Error wrong password")
+                continue
+        pass
+
