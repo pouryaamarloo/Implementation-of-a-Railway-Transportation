ccb0afb066a396bea80c9d594c65f48943bd6292
pourya <pouryaamarloo162@gmail.com>
2025-09-20 16:40:45 +0330

    taghirati ke pourya vared karde

--- Normal_User_Panel.py
+++ Normal_User_Panel.py
@@ -1,13 +1,13 @@
 import re #چون باید باید از Regex استفاده بشه
 
 class Normal_User_Panel():
-    def __init__(self , name , email , username , password):
-        self.name = name
-        self.email = email
-        self.username = username
-        self.password = password
+    def __init__(self ):
+        self.name = ""
+        self.email = ""
+        self.username = ""
+        self.password = ""
 
-        Users = {} #برای ذخیره کردن کاربر
+        self.Users = [] #برای ذخیره کردن کاربر
     #باید قبل از گرفتن اطلاعات با ریجکس ایمیل و یوزرنیم و پسورد رو بررسی کنیم
     def Acceptable_Email(email) :
         patern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
