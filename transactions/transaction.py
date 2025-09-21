a242b2902689a3eef9b4b32ba33d2fa42b5927c8
pourya <pouryaamarloo162@gmail.com>
2025-09-21 18:27:36 +0330

    edit transaction

--- transactions/transaction.py
+++ transactions/transaction.py
@@ -29,18 +29,35 @@ class Buy_Ticket(Normal_User_Panel):
             return
         for i in self.detail:
             print(f"train name = {i['train_name']} : train_id = {i['id']},\nline name {i['line_name']},\n price : {i['price']}  amount : {i['amount']} ")
-        print("please enter train id ")
-        choice = check_int()
-        print("Please enter the number of tickets you want.")
-        Count = check_int()
-        print(f"The amount of money in your wallet is: {self.wallet}")
-        for i in self.detail:
-            if choice == i['id']:
-                price = i['price']
-            else:
+        while True:
+            print("please enter train id ")
+            choice = check_int()
+            print("Please enter the number of tickets you want.")
+            Count = check_int()
+            price = 0
+            print(f"The amount of money in your wallet is: {self.wallet}")
+            for i in self.detail:
+                if choice == i['id']:
+                    price = i['price']
+                    if Count > i['amount']:
+                        print("The train capacity is less than what you selected\nif you want to back last panel enter 0")
+                        Count = pyip.inputInt(
+                            prompt="Please enter the number of tickets you want: ",
+                            min=0,
+                            max=i['amount']
+                        )
+                        if Count == 0 :
+                            self.show_data()
+                            return
+                        else :
+                            break
+                    else :
+                        break
+            if price==0 :
                 print("The ID you’re looking for was not found.")
-
-        price = price * Count
+            else :
+                price = price * Count
+                break
         if price > self.wallet:
             print("You don't have enough money.")
             again = pyip.inputYesNo(prompt=" Do you want to increase your wallet balance? (y/n) ")
@@ -54,6 +71,8 @@ class Buy_Ticket(Normal_User_Panel):
                 user.deposit(depos)
                 self.buy_ticket()
                 return
+        else:
+
 
     def edit_detail(self):
         pass
