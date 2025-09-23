62f39a63de3531d65c0e47c6963f3c269320c1f2
pourya <pouryaamarloo162@gmail.com>
2025-09-23 16:33:01 +0330

    edit panel 1

--- start_panel.py
+++ start_panel.py
@@ -1,36 +1,39 @@
-#from admin_kol import *
+from admin_kol import ManagementPanel
 from train_employe import *
+
 
 def start_panel():
     '''
-    پنل شروع جهت فراخوانی پنل های متفاوت از قبیل
-    ادمین کل ، کارمند قطار، کاربر عادی
+    Start Panel for Calling Different Panels
+    such as Admin, Train Employee, and Regular User
     '''
     state = 0
     while True:
-        print("1.ادمین کل")
-        print("2.کارمند قطار")
-        print("3.کاربر عادی")
-        print("4.خروج")
-        user_input = input()
+        print("""
+1.Admin Panel
+2.Employee Panel
+3.Normal Panel
+4.Exit
+        """)
+        user_input = input("please enter your choice: ")
+        while True:
+            if user_input == "1":
 
-        if user_input == "1":
-            user_name = input()
-            password = input()
-            if user_name == "Admin_Train":  # اگر ورودی 1 بود و یوزر نیم صحیح بود وارد پنل ادمین کل شود
-                state = 1
-            else:
-                print("Error wrong user name")  # در صورت اشتباه بودن یوزر نیم این پیام نمایش داده شود
-                continue
-            if password == "Pass_Train" and state == 1:  #:اگر پسورد تعیین شده بود و یوزر نیم درست وارد شده بود
-                print("welcome to the Management Panel!")  # نمایش پیام خوش آمد گویی
-                #admin = ManagementPanel()  # فراخوانی کلاس ادمین کل
-                #admin.panel()
-                continue
-
-            else:
-                print("Error wrong password")  # در صورت اشتباه بودن پسورد پیام مقابل نمایش داده شود
-                continue
+                user_name = input("please enter your user name: ")
+                password = input("please enter your password: ")
+                if user_name == "Admin_Train":  # اگر ورودی 1 بود و یوزر نیم صحیح بود وارد پنل ادمین کل شود
+                    state = 1
+                else:
+                    print("Error wrong user name")  # در صورت اشتباه بودن یوزر نیم این پیام نمایش داده شود
+                    continue
+                if password == "Pass_Train" and state == 1:  #:اگر پسورد تعیین شده بود و یوزر نیم درست وارد شده بود
+                    print("welcome to the Management Panel!")  # نمایش پیام خوش آمد گویی
+                    admin = ManagementPanel()  # فراخوانی کلاس ادمین کل
+                    admin.panel()
+                    break
+                else:
+                    print("Error wrong password")  # در صورت اشتباه بودن پسورد پیام مقابل نمایش داده شود
+                    continue
         if user_input == "2":
             while True:
                 user_name = input()
@@ -55,3 +58,4 @@ def start_panel():
         if user_input == "3":
             pass
 
+start_panel()
